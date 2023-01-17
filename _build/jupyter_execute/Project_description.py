b18d249878236bdfa7f502c2bb4374193fdc101a
Mohamed Badr <mhbadr94@gmail.com>
2023-01-17 12:07:11 +0100

    update project desc

---
+++ _build/jupyter_execute/Project_description.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Project Description
+
+# ## Background 
+# 
+# Your are required to conduct your own IO analysis for the final project of this course. You are free to choose the topic, and send it to the teaching staff for approval. The project submission must include a report and the python script used for the analysis. The report should be 2-4 pages exluding references. 
+
+# ## Report Structure
+# 
+# <ol>
+#   <li>Introduction/ Background</li>
+#   <li>Literature Review</li>
+#   <li>Methodology and Data</li>
+#   <li>Results</li>
+#   <li>Conclusion</li>
+#   <li>References</li>
+# </ol>
+
+# ## Data
+# 
+# You are free to choose any IO or Supply-Use data that fits your research question. However, for EE-MRIO we strongly reccomend the use of EXIOBASE which is developed at the IndEcol programme, NTNU. More on the EXIOBASE can be found on the [EXIOBASE website](https://www.exiobase.eu/).
+
+# 
