715a9e6f02d3dfa6a10c1ee306421d2581f809ff
Mohamed Badr <mhbadr94@gmail.com>
2023-03-15 10:40:44 +0100

    added latex to toc

---
+++ _build/jupyter_execute/Latex.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Using Latex in Markdwon files
+# Mohamed Badr
+
+# You can use a latex like equations in markdown blocks by using the dollar sign ($$) before and after the equation. Find examples bellow: 
+
+# $$
+#     \int_0^\infty \frac{x^3}{e^x-1}\,dx = \frac{\pi^4}{15}
+# $$
+
+# Latex equations can also be written in the format of \begin and \end statements
+
+# \begin{gather*}
+# a_1=b_1+c_1\\
+# a_2=b_2+c_2-d_2+e_2
+# \end{gather*}
+# 
+# \begin{align}
+# a_{11}& =b_{11}&
+#   a_{12}& =b_{12}\\
+# a_{21}& =b_{21}&
+#   a_{22}& =b_{22}+c_{22}
+# \end{align}
+
+# 
